78b6183c1368cf6eea18c146732a44b6c74492a7
omnya zerghouni <omnyazer@gmail.com>
2025-12-15 16:58:54 +0100

    activité 6

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
 
 Route::get("/", [IndexController::class, "index"]);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
