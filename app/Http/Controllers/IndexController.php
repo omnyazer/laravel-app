de43105d3afe71e402bbceaeac816d7e6f3d60a4
omnya zerghouni <omnyazer@gmail.com>
2025-12-16 15:30:29 +0100

    activité 12

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,8 +6,26 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function index()
-    {
-        return view('welcome', ['name' => 'Omnya']);
-    }
+   public function index()
+{
+    $articles = [
+        [
+            'title' => 'L’IA soigne mieux',
+            'description' => 'L’intelligence artificielle aide les médecins à diagnostiquer plus vite.',
+        ],
+        [
+            'title' => 'Villes vertes',
+            'description' => 'Les métropoles deviennent plus écologiques et durables.',
+        ],
+        [
+            'title' => 'Télétravail',
+            'description' => 'Plus de liberté, mais aussi plus de solitude.',
+        ],
+    ];
+
+    return view('welcome', [
+        'name' => 'Omnya',
+        'articles' => $articles,
+    ]);
+}
 }
