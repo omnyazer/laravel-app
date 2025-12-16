cc7869639e99dd3059a89e56cc6e4c2c9ae9c275
omnya zerghouni <omnyazer@gmail.com>
2025-12-16 14:55:06 +0100

    activité 11

---
+++ app/View/Components/Article.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Article extends Component
+{
+    public string $title;
+    public string $description;
+
+    public function __construct(string $title, string $description)
+    {
+        $this->title = $title;
+        $this->description = $description;
+    }
+
+    public function render()
+    {
+        return view('components.article');
+    }
+}
